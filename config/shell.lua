e3dc63d4e4307331e726cec1d72dd29e12ea682b
perry <perrypengwang@gmail.com>
2026-07-21 11:35:08 +0800

    refactor: 优化 Telnet 目标配置，修改编码

--- config/shell.lua
+++ config/shell.lua
@@ -4,7 +4,7 @@ local M = {}
 
 function M.apply(config)
     if utils.is_windows() then
-        config.default_prog = { "powershell.exe", "-NoLogo" }
+        config.default_prog = { "powershell.exe", "-NoLogo", "-NoExit", "-Command", "[Console]::OutputEncoding = [Console]::InputEncoding = [Text.Encoding]::UTF8" }
     else
         -- Unix 系统：zsh → bash
         if utils.unix_command_exists("zsh") then
